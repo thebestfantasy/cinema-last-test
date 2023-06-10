833867b6f60dda24a8563e74bd0f01db1e8e462a
thebestfantasy <sergej.fronch@gmail.com>
2023-06-10 19:58:39 +0300

    remove uk

--- src/fetch.js
+++ src/fetch.js
@@ -3,7 +3,7 @@ import { api } from "./api";
 
 export async function upcomingMovieRequest() {
     try {
-        const response = await api.get('/discover/movie?api_key=5e7ddcd8633dca4662efc3d617b08e4a&language=uk&sort_by=popularity.desc');
+        const response = await api.get('/discover/movie?api_key=5e7ddcd8633dca4662efc3d617b08e4a&sort_by=popularity.desc');
         return response.data;
     } catch (err) {
         console.log(err);
@@ -12,7 +12,7 @@ export async function upcomingMovieRequest() {
 
 export async function upcomingMovieGenreRequest() {
     try {
-        const response = await api.get('/genre/movie/list?api_key=5e7ddcd8633dca4662efc3d617b08e4a&language=uk');
+        const response = await api.get('/genre/movie/list?api_key=5e7ddcd8633dca4662efc3d617b08e4a');
         return response.data.genres;
     } catch (err) {
         console.log(err);
