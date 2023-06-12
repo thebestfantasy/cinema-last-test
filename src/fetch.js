209d086a01a819d7a2c67c15ba69f122d1267dfa
thebestfantasy <sergej.fronch@gmail.com>
2023-06-12 07:59:37 +0300

    changes

--- src/fetch.js
+++ src/fetch.js
@@ -1,20 +1,23 @@
-
-import { api } from "./api";
+import { api } from './api';
 
 export async function upcomingMovieRequest() {
-    try {
-        const response = await api.get('/discover/movie?api_key=5e7ddcd8633dca4662efc3d617b08e4a&sort_by=popularity.desc');
-        return response.data;
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const response = await api.get(
+      '/movie/upcoming?api_key=5e7ddcd8633dca4662efc3d617b08e4a&sort_by=popularity.desc'
+    );
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export async function upcomingMovieGenreRequest() {
-    try {
-        const response = await api.get('/genre/movie/list?api_key=5e7ddcd8633dca4662efc3d617b08e4a');
-        return response.data.genres;
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const response = await api.get(
+      '/genre/movie/list?api_key=5e7ddcd8633dca4662efc3d617b08e4a'
+    );
+    return response.data.genres;
+  } catch (err) {
+    console.log(err);
+  }
 }
